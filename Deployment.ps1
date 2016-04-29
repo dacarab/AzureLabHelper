60acf5af95a949e3f3ffd0d6a23394f9ef328138
David Carabott <David Carabott>
2016-04-29 10:17:14 +0100

    Added networking

--- Deployment.ps1
+++ Deployment.ps1
@@ -3,6 +3,8 @@
 $resourceGroup = "psod-iaas"
 $location = "North Europe"
 $storageAccountName = "psodiaas"
+$vnetName = "iaas-net"
+$nicName = "vm1-nic"
 #endregion
 
 # Login to Azure 
@@ -13,7 +15,21 @@ New-AzureRmResourceGroup -Name $resourceGroup -Location $location
 
 # New storage account
 New-AzureRmStorageAccount -Name $storageAccountName -ResourceGroupName $resourceGroup `
- -Type Standard_LRS -Location $location
+    -Type Standard_LRS -Location $location
+
+# Add networking
+$subnet = New-AzureRmVirtualNetworkSubnetConfig -name frontendSubnet -AddressPrefix 10.0.1.0/24
+$vnet = New-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroup -Location $location `
+    -AddressPrefix 10.0.0.6/16 -Subnet $subnet
+
+# Setup nic & public address
+$pip = New-AzureRmPublicIpAddress -Name $nicName -ResourceGroupName $resourceGroup `
+    -Location $location -AllocationMethod Dynamic
+
+$nic = New-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $resourceGroup `   
+    -Location $location -SubnetId $vnet.Subnets[0].ID -PublicIpAddressId $pip.Id
+
+
 
 
   
