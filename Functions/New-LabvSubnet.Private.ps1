9fe80690ac495d93280c17078f005cbcd2b1addd
David Carabott <david@carabott.me>
2016-08-21 21:08:12 +0100

    Actually this was

--- Functions/New-LabvSubnet.Private.ps1
+++ Functions/New-LabvSubnet.Private.ps1
@@ -11,6 +11,16 @@
 Function New-LabvSubnet{
     [CmdletBinding()]
     Param(
+        $vNetwork,
         $ResourceGroup
     )
+
+    # Find a spare range in the passed network in the 10.0.255.0 range
+    $LastUsedSubnet = $vNetwork.Subnets | Where-Object AddressPrefix -like "10.255.*" |
+      Select-Object -ExcludeProperty AddressPrefix |Sort-Object |Select-Object -Last 1
+    
+    # Modify the config of the vNet to include new subnet
+    $subnet = New-AzureRmVirtualNetworkSubnetConfig -name frontendSubnet -AddressPrefix 10.0.1.0/24
+    $vnet = New-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroup -Location $location `
+      -AddressPrefix 10.0.0.0/16 -Subnet $subnet
 }
