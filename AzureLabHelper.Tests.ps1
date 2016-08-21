d63e65770fb03b3f0d873e83ec36c3404ab119af
David Carabott <david@carabott.me>
2016-08-21 16:29:17 +0100

    Fixed New-LabRG return type test

--- AzureLabHelper.Tests.ps1
+++ AzureLabHelper.Tests.ps1
@@ -1,3 +1,9 @@
+$Module = "AzureLabHelper"
+
 Import-Module Pester
-Invoke-Pester ".\Tests\AzureLabHelper.Module.Tests.ps1"
+
+Get-Module $Module | Remove-Module -Force
+Import-Module $PSScriptRoot\$Module.psm1 -Force
+
+Invoke-Pester ".\Tests\$Module.Module.Tests.ps1"
 Invoke-Pester ".\Tests\New-LabRG.Tests.ps1" -Tag "Unit"
--- Tests/New-LabRG.Tests.ps1
+++ Tests/New-LabRG.Tests.ps1
@@ -2,32 +2,29 @@ $Module = "AzureLabHelper"
 $Function = "New-LabRG"
 $Here = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-Get-Module $Module | Remove-Module -Force
-Import-Module $Here\..\$Module.psm1 -Force
-
 Describe "$Function Unit Tests" -Tags "Unit"{
     InModuleScope AzureLabHelper{
         Context "$Function returns the expected object" {
         
             Mock New-AzureRmResourceGroup {
                 $MockData = @'
-        <Objs Version="1.1.0.1" xmlns="http://schemas.microsoft.com/powershell/2004/04">
-        <Obj RefId="0">
-            <TN RefId="0">
-            <T>Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels.PSResourceGroup</T>
-            <T>System.Object</T>
-            </TN>
-            <ToString>Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels.PSResourceGroup</ToString>
-            <Props>
-            <S N="ResourceGroupName">PesterTest0001</S>
-            <S N="Location">northeurope</S>
-            <S N="ProvisioningState">Succeeded</S>
-            <Nil N="Tags" />
-            <Nil N="TagsTable" />
-            <S N="ResourceId">/subscriptions/f4a4040f-1f28-4c55-ae0c-b14ad0f38a87/resourceGroups/PesterTest0001</S>
-            </Props>
-        </Obj>
-        </Objs>    
+<Objs Version="1.1.0.1" xmlns="http://schemas.microsoft.com/powershell/2004/04">
+  <Obj RefId="0">
+    <TN RefId="0">
+      <T>Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels.PSResourceGroup</T>
+      <T>System.Object</T>
+    </TN>
+    <ToString>Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels.PSResourceGroup</ToString>
+    <Props>
+      <S N="ResourceGroupName">PesterTest0001</S>
+      <S N="Location">northeurope</S>
+      <S N="ProvisioningState">Succeeded</S>
+      <Nil N="Tags" />
+      <Nil N="TagsTable" />
+      <S N="ResourceId">/subscriptions/f4a4040f-1f28-4c55-ae0c-b14ad0f38a87/resourceGroups/PesterTest0001</S>
+    </Props>
+  </Obj>
+</Objs>   
 '@
                 $Output = [System.Management.Automation.PSSerializer]::Deserialize($MockData)
 
@@ -36,8 +33,9 @@ Describe "$Function Unit Tests" -Tags "Unit"{
 
             $ResourceGroup = New-LabRG -ResourceGroupPrefix "AcceptenceTest"
 
-            It "$Function returns a PSResourceGroup object" {
-                $ResourceGroup.GetType().Name | Should Be "PSResourceGroup"
+            It "$Function returns a PSResourceGroup object " {
+                $ResourceGroup.ToString() |
+                  Should Be "Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels.PSResourceGroup"
             }
         } # Context "$Function returns the expected object"
     } # InModuleScope AzureLabHelper
